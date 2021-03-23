4f4c7ede310c907df6a984e2ed4c861806d87c66
jimist <arash.jeem@gmail.com>
2021-03-23 05:35:41 +0430

    Timist initial struct

--- timist.go
+++ timist.go
@@ -1,1 +1,24 @@
 package timist
+
+import "time"
+
+type Timist struct {
+	*time.Time
+}
+
+func FromTime(t *time.Time) *Timist {
+	return &Timist{
+		t,
+	}
+}
+
+func Now() *Timist {
+	now := time.Now()
+	return &Timist{
+		&now,
+	}
+}
+
+func (ts *Timist) GetTime() *time.Time {
+	return ts.Time
+}
